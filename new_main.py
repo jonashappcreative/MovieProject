ba11c338bd401af6d3c22c5b3d88c350043d05f5
Jonas Happ <github@jonashapp.com>
2024-07-26 00:43:48 +0200

    Added Docstring for new_main.py and reformatted code

--- new_main.py
+++ new_main.py
@@ -1,9 +1,16 @@
 from movie_app import MovieApp
-from storage_json import StorageJson
 from storage_csv import StorageCsv
+from storage_json import StorageJson
+
 
 def choose_user():
+    """
+    A function that covers the initial choice of which user is using the Movie App.
+    The users are currently hard coded into a list of users, but can be later saved in a users database.
+    Each user is currently connected to its databases filepath.
 
+    :return: The Object of the Storage Type and Filepath that will be used in the MovieApp.
+    """
     users = ["user1", "user2", "user3", "user4"]
 
     print("\nPlease choose the user profile for the Movie App")
@@ -26,9 +33,19 @@ def choose_user():
         print("You choose User 4")
         storage = StorageCsv("movie_storage_database.csv")
 
+    else:
+        print("You choose User 1 as default")
+        storage = StorageJson("movie_storage_database.json")
+
     return storage
 
+
 def main():
+    """
+    Runs the MovieApp using the Storage Item choosen in the prior function.
+    Executed until run is stopped by the user by the bool running_choice.
+    :return: Nothing
+    """
     print("********** My Movies Database **********")
 
     storage = choose_user()
